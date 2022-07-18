de9913cfc7b0fa0cbe54c57d6d35abeb78ad1a50
HazelDaniel <hazeldaniel856@gmail.com>
2022-07-19 00:38:16 +0100

    Updates

--- src/components/search_box/search_box.component.jsx
+++
@@ -1,5 +0,0 @@
-import React from "react";
-import "./search_box.styles.css";
-export const SearchBox = props => {
-	return <input className="search-box" type="search" placeholder={props.placeholder} onChange={props.handler} />;
-}
